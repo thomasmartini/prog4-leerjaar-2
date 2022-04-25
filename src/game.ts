60cc0fa3132af0fa1773d72cbff79346699e846d
KokoDoko <erik.katerborg@gmail.com>
2022-04-25 12:49:00 +0200

    remove static

--- src/game.ts
+++ src/game.ts
@@ -1,16 +1,26 @@
 import * as PIXI from 'pixi.js'
+import fishImage from "./images/fish.png"
+import bubbleImage from "./images/bubble.png"
+import waterImage from "./images/water.jpg"
 
-// create a pixi canvas
+//
+// STAP 1 - maak een pixi canvas
+//
 const pixi = new PIXI.Application({ width: 800, height: 450 })
 document.body.appendChild(pixi.view)
 
-// preload all our textures
+//
+// STAP 2 - preload alle afbeeldingen
+//
 const loader = new PIXI.Loader()
-loader.add('fishTexture', "./assets/images/fish.png")
-      .add('bubbleTexture', "./assets/images/bubble.png")
+loader.add('fishTexture', fishImage)
+      .add('bubbleTexture', bubbleImage)
+      .add('waterTexture', waterImage)
 loader.load(()=>loadCompleted())
 
-// after loading is complete, create a fish sprite
+//
+// STAP 3 - maak een sprite als de afbeeldingen zijn geladen
+//
 function loadCompleted() {
     let fish = new PIXI.Sprite(loader.resources["fishTexture"].texture!)
     pixi.stage.addChild(fish)
