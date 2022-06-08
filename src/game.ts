d54b8273c170f4a80fb5f0c41ca5ea63052a10ef
thoma <thomasmartini1999@gmail.com>
2022-06-08 14:53:43 +0200

    week 2 opdracht 2, sprite array en text

--- src/game.ts
+++ src/game.ts
@@ -4,11 +4,14 @@ import bgImage from "./images/water.jpg"
 import bubbleImage from "./images/bubble.png"
 
 export class Game {
-
+    spritesFish : PIXI.Sprite[] = []
+    spritesBubbles: PIXI.Sprite[] = []
     pixi: PIXI.Application
-    fish:PIXI.Sprite
+    style : PIXI.TextStyle
+    fish: PIXI.Sprite
+    graphics: PIXI.Graphics
     bubble:PIXI.Sprite
-    anotherFish:PIXI.Sprite
+    basictext: PIXI.Text
     anotherBubble:PIXI.Sprite
     background:PIXI.Sprite
     loader:PIXI.Loader
@@ -16,6 +19,7 @@ export class Game {
     constructor() {
         this.pixi = new PIXI.Application({ width: 900, height: 500 })
         document.body.appendChild(this.pixi.view)
+        
 
         this.loader = new PIXI.Loader()
         this.loader
@@ -31,62 +35,76 @@ export class Game {
         return randomX
     }
     randomNumberY(){
-        let randomY = Math.floor(Math.random() * 500)
+        let randomY = Math.floor(Math.random() * 500 - this.fish.height)
         return randomY
     }
-
-    doneLoading() {
-        console.log("all textures loaded!")
+    doneLoading(){
         this.background = new PIXI.Sprite(this.loader.resources["backgroundTexture"].texture!)
         this.pixi.stage.addChild(this.background)
 
-        this.fish = new PIXI.Sprite(this.loader.resources["fishTexture"].texture!)
-        this.fish.x = this.randomNumberX()
-        this.fish.y = this.randomNumberY()
-        this.pixi.stage.addChild(this.fish)
-
-        this.anotherFish = new PIXI.Sprite(this.loader.resources["fishTexture"].texture!)
-        this.anotherFish.x = this.randomNumberX()
-        this.anotherFish.y = this.randomNumberY()
-        this.pixi.stage.addChild(this.anotherFish)
-
-        this.bubble = new PIXI.Sprite(this.loader.resources["bubbleTexture"].texture!)
-        this.bubble.x = this.randomNumberX()
-        this.bubble.y = this.randomNumberY()
-        this.pixi.stage.addChild(this.bubble)
-
-        this.anotherBubble = new PIXI.Sprite(this.loader.resources["bubbleTexture"].texture!)
-        this.anotherBubble.x = this.randomNumberX()
-        this.anotherBubble.y = this.randomNumberY()
-        this.pixi.stage.addChild(this.anotherBubble)
-
-        this.pixi.ticker.add((delta) => this.update(delta))
-        
-    }
-
-    update(delta : number) {
-        this.fish.x -= 2
-        this.bubble.y -= 1.5
-        this.anotherBubble.y -= 1.5
-        this.anotherFish.x -= 3
-
-        if(this.fish.x <= -100) {
-            this.fish.x = 900
+        for (let i = 0; i<10; i++) {
+            this.fish = new PIXI.Sprite(this.loader.resources["fishTexture"].texture!)
+            this.fish.x = this.randomNumberX()
             this.fish.y = this.randomNumberY()
-        }
-        if(this.anotherFish.x <= -100) {
-            this.anotherFish.x = 900
-            this.anotherFish.y = this.randomNumberY()
-        }
-        if(this.bubble.y <= -40){
+            this.pixi.stage.addChild(this.fish)
+            this.spritesFish.push(this.fish)
+
+            this.bubble = new PIXI.Sprite(this.loader.resources["bubbleTexture"].texture!)
             this.bubble.x = this.randomNumberX()
-            this.bubble.y = 500
+            this.bubble.y = this.randomNumberY()
+            this.pixi.stage.addChild(this.bubble)          
+            this.spritesBubbles.push(this.bubble)
+            this.pixi.ticker.add((delta) => this.update(delta))
+
         }
-        if(this.anotherBubble.y <= -40){
-            this.anotherBubble.x = this.randomNumberX()
-            this.anotherBubble.y = 500
+        this.style = new PIXI.TextStyle({
+            fontFamily: 'Arial',
+            fontSize: 36,
+            fontStyle: 'italic',
+            fontWeight: 'bold',
+            fill: ['#ffffff', '#00ff99'], // gradient
+            stroke: '#4a1850',
+            strokeThickness: 5,
+            dropShadow: true,
+            dropShadowColor: '#000000',
+            dropShadowBlur: 4,
+            dropShadowAngle: Math.PI / 6,
+            dropShadowDistance: 6,
+            wordWrap: true,
+            wordWrapWidth: 440,
+            lineJoin: 'round',})
+
+            this.basictext = new PIXI.Text('wow cool fish ', this.style)
+            this.basictext.x = 350
+            this.basictext.y = 250
+            this.pixi.stage.addChild(this.basictext)
+
+            this.graphics = new PIXI.Graphics()
+
+            this.graphics.beginFill(0xDE3249);
+            this.graphics.drawRect(50, 50, 100, 100);
+            this.graphics.endFill();
+
+            this.pixi.stage.addChild(this.graphics)
+    }
+   
+    update(delta : number) {
+        for(let sprite of this.spritesFish){
+            sprite.x -= 1 * delta
+            if(sprite.x <= 0 - sprite.width){
+                sprite.x = 900
+                sprite.y = this.randomNumberY()
+            }
+        }
+        for(let sprite of this.spritesBubbles){
+            sprite.y -= 1 * delta
+            if(sprite.y <= 0 - sprite.height){
+                sprite.x = this.randomNumberX()
+                sprite.y = 500 + sprite.height
+            }
         }
     }
 }
+
 
 new Game()
